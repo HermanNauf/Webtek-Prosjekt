da6b587458a1c8cbef5e6137bb1ff54ad1b5f65a
AR113 <herman.nauf@hotmail.com>
2021-04-20 12:00:00 +0200

    the webpage will now show items in the itemlist

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import {Switch, Route } from 'react-router-dom';
-=======
-import { Switch, Route } from 'react-router-dom';
->>>>>>> 331ea0a264e8b7941920e2b841f9ef2fa696d1aa
 import Navbar from "./components/Navbar/Navbar";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
@@ -15,7 +11,6 @@ import React, { useEffect } from "react";
 // Pages
 import Home from "./pages/Home/Home";
 import Newitem from "./pages/Newitem/Newitem"
-<<<<<<< HEAD
 import { itemlist } from "./lists/itemlist";
 import { setItems } from "./actions/actions";
 export default function App() {
@@ -24,16 +19,6 @@ export default function App() {
   useEffect(() => {
     dispatch(setItems(itemlist));
   }, [dispatch]);
-=======
-import { setSearch } from "./actions/searchActions";
-
-export default function App() {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(setSearch(""));
-    }, [dispatch]);
->>>>>>> 331ea0a264e8b7941920e2b841f9ef2fa696d1aa
 
   return (
     <div className="App">
