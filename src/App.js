b2e066c2d84d0ba3c5f337065629634d140b8c64
AR113 <herman.nauf@hotmail.com>
2021-04-20 11:02:27 +0200

    started working on the app.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { Switch, Route } from "react-router-dom";
 
-function App() {
+
+// Pages
+import Home from "./pages/Home/Home";
+import Newitem from "./pages/Newitem/Newitem"
+export default function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/new" component={Newitem} />
+        <Route path="/detail/:id" component={Detail} />
+      </Switch>
+
     </div>
   );
 }
-
-export default App;
