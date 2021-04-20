87dcf25d99bec2ea15573ad173910ebf0b8113db
AR113 <herman.nauf@hotmail.com>
2021-04-20 11:22:01 +0200

    added a dummy list

---
+++ itemlist.js
@@ -0,0 +1,14 @@
+export const itemlist = [
+    {
+        id: "0",
+        name: "Sko",
+        description: "Raske sko",
+        brand: "Nike"
+    },
+    {
+        id: "1",
+        name: "Briller",
+        description: "Raske briller",
+        brand: "Northug"
+    },
+    ]
