5d30d37ff0567e3cbb6a88c29f2a078016d8ae5e
AR113 <herman.nauf@hotmail.com>
2021-04-28 11:00:52 +0200

    added a fetch request

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Item from "../../components/items/item";
+import { useState } from "react";
+import { useEffect} from "react";
+import axios, * as others from 'axios';
+
 
 export default function Home({search}){
     const items = useSelector((state) => state.items);
+    const {data, setData} = useState(null);
 
+    useEffect(() => {
+        axios("http://localhost:8080/api/product/products",{ mode: 'no-cors'})
+        .then(response => {
+        setData(response.data) 
+     }).catch(error => {
+            console.log(error)
+        })
+    },[])
     let filterItems = (item) => {
         return item.name.toLowerCase().includes(search.toLowerCase())
             || item.description.toLowerCase().includes(search.toLowerCase())
